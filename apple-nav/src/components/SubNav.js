1ce1ea9d88cbcf28fe39b5e4e4ff1212bbdb6e04
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2019-10-02 22:47:47 -0400

    Created the Mac function in SubNav

--- apple-nav/src/components/SubNav.js
+++ apple-nav/src/components/SubNav.js
@@ -13,8 +13,40 @@ export function Home() {
 export function Mac() {
   return (
     <div className="sub-nav">
+
       <img src="https://www.apple.com/v/mac/home/ai/images/familybrowser/macbookair__ej39du0gz4uq_large.svg" />
       <a href="#">MacBook Air</a>
+      <span>New</span>
+
+      <img src="https://www.apple.com/v/mac/home/ai/images/familybrowser/macbookpro__enfip2k1cxkm_large.svg" />
+      <a href="#">MacBook Pro</a>
+      <span>New</span>
+
+      <img src="https://www.apple.com/v/mac/home/ai/images/familybrowser/imac__dkswmjfqeziq_large.svg" />
+      <a href="#">iMac</a>
+
+      <img src="https://www.apple.com/v/mac/home/ai/images/familybrowser/imac_pro__eer4bfwlutme_large.svg" />
+      <a href="#">iMac Pro</a>
+
+      <img src="https://www.apple.com/v/mac/home/ai/images/familybrowser/mac_pro__bn92faz71k6a_large.svg" />
+      <a href="#">Mac Pro</a>
+      <span>New</span>
+
+      <img src="https://www.apple.com/v/mac/home/ai/images/familybrowser/macmini__c6u0it831y0y_large.svg" />
+      <a href="#">Mac mini</a>
+
+      <img src="https://www.apple.com/v/mac/home/ai/images/familybrowser/mac_comp__ee2pokmcxh26_large.svg" />
+      <a href="#">Compare</a>
+
+      <img src="https://www.apple.com/v/mac/home/ai/images/familybrowser/pro_display_xdr__do0vjpny1kqe_large.svg" />
+      <a href="#">Pro Display XDR</a>
+      <span>New</span>
+
+      <img src="https://www.apple.com/v/mac/home/ai/images/familybrowser/mac_acc__fy6vtx5eqoyi_large.svg" />
+      <a href="#">Accessories</a>
+
+      <img src="https://www.apple.com/v/mac/home/ai/images/familybrowser/mac_acc__fy6vtx5eqoyi_large.svg" />
+      <a href="#">Catalina</a>
     </div>
   );
 }
